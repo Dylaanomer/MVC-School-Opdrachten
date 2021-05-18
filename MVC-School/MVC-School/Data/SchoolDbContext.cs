70befa1e7f62b547255c302fc791532de7cf09ac
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2021-05-18 17:02:38 +0200

    Initial commit

---
+++ MVC-School/MVC-School/Data/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_School.Models;
+
+namespace MVC_School.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+
+        public DbSet<Student> Studenten { get; set; }
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
